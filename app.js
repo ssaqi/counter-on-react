4a2f224674331f32ad39bc30d2bfda91d7f55bc6
Muhammad Saqib <89692631+ssaqi@users.noreply.github.com>
2022-11-06 21:54:16 +0500

    counter

---
+++ app.js
@@ -0,0 +1,15 @@
+var val = 0;
+function App(){
+val +=1;
+
+document.getElementById("val").innerHTML = val;
+
+}
+
+
+function clearApp(){
+val="0";
+document.getElementById("val").innerHTML = val;
+
+}
+
